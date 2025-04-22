38233dbc84156e8b0ce9a0c40cd46dd67fdf5b70
Nishant Sirwani <nishantsirwani@gmail.com>
2025-04-22 16:30:01 +0530

    Added Records Product Request and Product Response

--- src/main/java/com/sirwani/microservices/product/Controller/ProductController.java
+++ src/main/java/com/sirwani/microservices/product/Controller/ProductController.java
@@ -3,6 +3,7 @@ package com.sirwani.microservices.product.Controller;
 import com.sirwani.microservices.product.Model.Product;
 import com.sirwani.microservices.product.Service.ProductService;
 import com.sirwani.microservices.product.dto.ProductRequest;
+import com.sirwani.microservices.product.dto.ProductResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,13 +19,13 @@ public class ProductController {
 
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
-    public Product createProduct(@RequestBody ProductRequest productRequest) {
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
         return productService.createProduct(productRequest);
     }
 
     @GetMapping("/get-all-products")
     @ResponseStatus(HttpStatus.OK)
-    public List<Product> fetchAllProducts() {
+    public List<ProductResponse> fetchAllProducts() {
         return productService.fetchAllProducts();
     }
 }
--- src/main/java/com/sirwani/microservices/product/Service/ProductService.java
+++ src/main/java/com/sirwani/microservices/product/Service/ProductService.java
@@ -3,6 +3,7 @@ package com.sirwani.microservices.product.Service;
 import com.sirwani.microservices.product.Model.Product;
 import com.sirwani.microservices.product.Repo.ProductRepo;
 import com.sirwani.microservices.product.dto.ProductRequest;
+import com.sirwani.microservices.product.dto.ProductResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,7 @@ public class ProductService {
     @Autowired
     private ProductRepo productRepo;
 
-    public Product createProduct(ProductRequest productRequest) {
+    public ProductResponse createProduct(ProductRequest productRequest) {
         Product product = Product.builder()
                 .name(productRequest.name())
                 .description(productRequest.description())
@@ -24,11 +25,21 @@ public class ProductService {
                 .build();
 
         log.info("Product added successfully!");
-        return productRepo.save(product);
-//        return product;
+        productRepo.save(product);
+        return new ProductResponse(product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getPrice());
     }
 
-    public List<Product> fetchAllProducts() {
-        return productRepo.findAll();
+    public List<ProductResponse> fetchAllProducts() {
+        return productRepo.findAll()
+                .stream()
+                .map(product -> new ProductResponse(
+                        product.getId(),
+                        product.getName(),
+                        product.getDescription(),
+                        product.getPrice()))
+                .toList();
     }
 }
